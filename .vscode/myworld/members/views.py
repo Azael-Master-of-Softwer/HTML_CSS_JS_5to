40705ac4238e5b19f555dc01e994af3648e260c6
Azael-Master-of-Softwer <azaelcardenasc@uanl.edu.mx>
2025-11-07 14:38:58 -0600

    django migration file for initial model creation

--- .vscode/myworld/members/views.py
+++ .vscode/myworld/members/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def index(request):
+    mymembers = Member.objects.all().values()
     template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
-
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 # Create your views here.
